b1c685a3c7b96d7c4f7d96c4afa4daa4a7087d3e
Wambu-i7 <wambuindikiru@gmail.com>
2024-11-08 19:21:29 +0000

    minimum operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module: min_operations
+
+This module provides a function to calculate the minimum number of operations 
+needed to reach exactly 'n' 'H' characters in a text file, starting with one 'H'
+"""
+
+def minOperations(n):
+    """
+    Returns the fewest operations needed to get exactly 'n' 'H' characters in a 
+    file, starting from one 'H'.
+
+    Parameters:
+    n (int): Target number of 'H' characters.
+
+    Returns:
+    int: Minimum number of operations to reach 'n' characters, or 0 if n <= 1.
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    divisor = 2
+    
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+        
+    return operations
