685b8291544f152cb48c2b11ad0588d253231a81
Wambu-i7 <wambuindikiru@gmail.com>
2024-11-08 19:03:18 +0000

    pascals

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -46,7 +46,7 @@ def pascal_triangle(n):
         row = [1]
 
 
-        for j in range(1, i):
+    for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
 
         # End each row with 1
