5721808d4a6abf882009029f88dc9aadb40ddf1f
Wambu-i7 <wambuindikiru@gmail.com>
2024-12-09 07:06:31 +0000

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""prime Game"""
+def isWinner(x, nums):
+    """Determines the winner of the prime Game"""
+
+    if x < 1 or not nums:
+        return None
+
+    # Determine the maximum n for prime sieve
+    max_n = max(nums)
+    
+    # Create a sieve to find all primes up to max_n
+    sieve = [True] * (max_n + 1)
+    sieve[0] = sieve[1] = False  # 0 and 1 are not prime
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_n + 1, i):
+                sieve[j] = False
+
+    # Precompute the number of primes up to each index
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if sieve[i] else 0)
+
+    # Determine winners for each round
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+
